b0c02ac1e35f6dee5fbbc6679ab86ab2064c89ad
lejdiprifti <priftilejdi@gmail.com>
2020-03-18 22:57:49 +0100

    validated position of the spaceship

--- src/js/game.js
+++ src/js/game.js
@@ -1,5 +1,7 @@
+import './spaceship.js'
 const template = document.createElement('template')
 template.innerHTML = `
+<div id="container"></div>
 `
 
 export class BattleGame extends window.HTMLElement {
@@ -8,10 +10,75 @@ export class BattleGame extends window.HTMLElement {
     this.attachShadow({ mode: 'open' })
     this.shadowRoot.appendChild(template.content.cloneNode(true))
     this.createArray()
+    this.moveSpaceship()
   }
 
   createArray () {
-    console.log(window.screen.width)
+    const columns = (window.screen.width / 64).toPrecision(2)
+    const rows = (window.screen.height / 64).toPrecision(2)
+    let gamingArray = new Array(Number(rows)).fill()
+    gamingArray = gamingArray.map(el => new Array(Number(columns)))
+    return gamingArray
+  }
+
+  moveSpaceship () {
+    const container = this.shadowRoot.querySelector('#container')
+
+    const spaceship = document.createElement('space-ship')
+    spaceship.style.position = 'absolute'
+    spaceship.style.top = 0
+    spaceship.style.left = 0
+    container.appendChild(spaceship)
+
+    let top = 0
+    let left = 0
+
+    document.addEventListener('keydown', () => {
+      switch (event.keyCode) {
+        case 39:
+          if (this.validatePostionLeft(left, event.keyCode) === true) {
+            spaceship.style.left = (left + 32) + 'px'
+            left += 32
+          }
+          break
+        case 37:
+          if (this.validatePostionLeft(left, event.keyCode) === true) {
+            spaceship.style.left = (left - 32) + 'px'
+            left -= 32
+          }
+          break
+        case 38:
+          if (this.validatePostionTop(top, event.keyCode) === true) {
+            spaceship.style.top = (top - 32) + 'px'
+            top -= 32
+          }
+          break
+        case 40:
+          if (this.validatePostionTop(top, event.keyCode) === true) {
+            spaceship.style.top = (top + 32) + 'px'
+            top += 32
+          }
+          break
+      }
+    })
+  }
+
+  validatePostionLeft (left, code) {
+    if (left < 1280 && code === 39) {
+      return true
+    } else if (left > 0 && code === 37) {
+      return true
+    }
+    return false
+  }
+
+  validatePostionTop (top, code) {
+    if (top > 0 && code === 38) {
+      return true
+    } else if (top < 544 && code === 40) {
+      return true
+    }
+    return false
   }
 }
 
